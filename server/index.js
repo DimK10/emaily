7d1991d6b61726cd2119ef942c7788a34fe38aab
DimK10 <james_k23@hotmail.gr>
2022-07-16 23:16:05 +0300

    Lesson 25 finished

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const {
+  googleClientID: clientID,
+  googleClientSecret: clientSecret,
+} = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID,
+      clientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
